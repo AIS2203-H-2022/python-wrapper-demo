5f01d8bac8a96b095e0b9155038bc8fee9e92de2
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-10-18 17:08:54 +0200

    update

--- pycpp/__init__.py
+++ pycpp/__init__.py
@@ -98,22 +98,3 @@ def CppLib():
     if _CppLib._instance is None:
         _CppLib._instance = _CppLib()
     return _CppLib._instance
-
-
-def main():
-    v1 = Vector3(1, 2, 3)
-    v2 = Vector3(1, 1, 1)
-    res = v1.add(v2)
-    print(res)
-
-    m = Matrix4()
-    m.identity()
-    print(m)
-    m.set_position(10, -5, 6)
-    print(m)
-
-    del m
-
-
-if __name__ == "__main__":
-    main()
--- pycpp/main.py
+++ pycpp/main.py
@@ -1,8 +1,19 @@
+from pycpp import Vector3, Matrix4
 
 
 def main():
-    #v1 = Vector3()
-    pass
+    v1 = Vector3(1, 2, 3)
+    v2 = Vector3(1, 1, 1)
+    res = v1.add(v2)
+    print(res)
+
+    m = Matrix4()
+    m.identity()
+    print(m)
+    m.set_position(10, -5, 6)
+    print(m)
+
+    del m
 
 
 if __name__ == "__main__":
